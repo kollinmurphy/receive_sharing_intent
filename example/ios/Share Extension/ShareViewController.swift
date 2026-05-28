184008007ae90b5ed0198e95c8e7b9f630106399
Joo Aun Saw <jasaw81@gmail.com>
2026-05-28 12:26:10 +1000

    chore: update ios example app

--- example/ios/Share Extension/ShareViewController.swift
+++ example/ios/Share Extension/ShareViewController.swift
@@ -6,7 +6,7 @@
 //  Created by Kasem Mohamed on 2019-05-30.
 //  Copyright © 2019 The Chromium Authors. All rights reserved.
 //
-import receive_sharing_intent
+import receive_sharing_intent_core
 
 class ShareViewController: RSIShareViewController {
     
